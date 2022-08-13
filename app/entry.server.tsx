26835e4d50ea5df2c0096071e92d5576685117b6
Tom Slutsky <tom@kokoapps.co.il>
2022-08-13 15:24:22 +0300

    bump versions

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,18 +1,28 @@
-import { RemixServer } from "@remix-run/react";
-import type { EntryContext } from "@remix-run/server-runtime";
 import { createInstance } from "i18next";
 import Backend from "i18next-fs-backend";
 import { resolve } from "node:path";
-import { renderToString } from "react-dom/server";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import { i18n } from "~/i18n.server";
 
+import { PassThrough } from "stream";
+import { renderToPipeableStream } from "react-dom/server";
+import { RemixServer } from "@remix-run/react";
+import { Response } from "@remix-run/node";
+import type { EntryContext, Headers } from "@remix-run/node";
+import isbot from "isbot";
+
+const ABORT_DELAY = 5000;
+
 export default async function handleRequest(
   request: Request,
-  statusCode: number,
-  headers: Headers,
-  context: EntryContext
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
 ) {
+  const callbackName = isbot(request.headers.get("user-agent"))
+    ? "onAllReady"
+    : "onShellReady";
+
   // First, we create a new instance of i18next so every request will have a
   // completely unique instance and not share any state
   let instance = createInstance();
@@ -20,7 +30,7 @@ export default async function handleRequest(
   // Then we could detect locale from the request
   let lng = await i18n.getLocale(request);
   // And here we detect what namespaces the routes about to render want to use
-  let ns = i18n.getRouteNamespaces(context);
+  let ns = i18n.getRouteNamespaces(remixContext);
 
   await instance
     .use(initReactI18next) // Tell our instance to use react-i18next
@@ -41,16 +51,36 @@ export default async function handleRequest(
 
   // Then you can render your app wrapped in the I18nextProvider as in the
   // entry.client file
-  let markup = renderToString(
-    <I18nextProvider i18n={instance}>
-      <RemixServer context={context} url={request.url} />
-    </I18nextProvider>
-  );
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  headers.set("Content-Type", "text/html");
+    const { pipe, abort } = renderToPipeableStream(
+      <I18nextProvider i18n={instance}>
+        <RemixServer context={remixContext} url={request.url} />
+      </I18nextProvider>,
+      {
+        [callbackName]() {
+          let body = new PassThrough();
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: statusCode,
-    headers: headers,
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              status: didError ? 500 : responseStatusCode,
+              headers: responseHeaders,
+            })
+          );
+          pipe(body);
+        },
+        onShellError(err: unknown) {
+          reject(err);
+        },
+        onError(error: unknown) {
+          didError = true;
+          console.error(error);
+        },
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
   });
 }
--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -1,11 +1,11 @@
 // Use this to create a new user and login with that user
 // Simply call this with:
-// node --require esbuild-register ./cypress/support/create-user.ts username@example.com
+// npx ts-node --require tsconfig-paths/register ./cypress/support/create-user.ts username@example.com
 // and it will log out the cookie value you can use to interact with the server
 // as that new user.
 
 import { parse } from "cookie";
-import { installGlobals } from "@remix-run/node/globals";
+import { installGlobals } from "@remix-run/node";
 import { createUserSession } from "~/session.server";
 import { createUser } from "~/models/user.server";
 
@@ -22,7 +22,7 @@ async function createAndLogin(email: string) {
   const user = await createUser(email, "myreallystrongpassword");
 
   const response = await createUserSession({
-    request: new Request(""),
+    request: new Request("test://test"),
     userId: user.id,
     remember: false,
     redirectTo: "/",
