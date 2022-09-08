d65c29a3ffc76b25c6b8751ad876f6821f5803e8
Tom Slutsky <tom@kokoapps.co.il>
2022-09-08 17:40:24 +0300

    reset-password-token migration

---
+++ prisma/migrations/20220908144013_reset_password_token/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Note` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Note" DROP CONSTRAINT "Note_userId_fkey";
+
+-- DropTable
+DROP TABLE "Note";
+
+-- CreateTable
+CREATE TABLE "ResetPasswordToken" (
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expirationDate" TIMESTAMP(3),
+    "redeemedAt" TIMESTAMP(3),
+    "ip" TEXT,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "ResetPasswordToken_pkey" PRIMARY KEY ("token")
+);
+
+-- AddForeignKey
+ALTER TABLE "ResetPasswordToken" ADD CONSTRAINT "ResetPasswordToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
